d3cc66e1f6748193d90abde229416c7238099968
ChrisSav713 <106366587+ChrisSav713@users.noreply.github.com>
2022-11-20 19:29:20 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React, { useEffect } from 'react'
 
 function App() {
+  const [myState, setMyState] = React.useState([])
+
+// useEffect(() => {
+//   fetch('https://swapi.dev/api/people')
+//   .then(response => response.json())
+//   .then(response => setMyState(response.results))
+//   .catch(() => setErr("Error fetching data"))
+// }, [])
+
+useEffect(() => {
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
+  .then(response =>  response.json())
+  .then(response => setMyState(response.results))
+}, [])
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {myState.length > 0 && myState.map((item, index) => {
+        return (<p key={index}>{item.name}</p>)
+      })}
     </div>
   );
 }
